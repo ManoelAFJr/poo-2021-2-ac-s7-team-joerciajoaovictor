8a405828d25e4808bffb5067b8a4d52f24dde6aa
João Victor Gomes <joao-iccz@hotmail.com>
2021-12-14 16:55:07 -0300

    Acionando questão 2 página 93

---
+++ atv2.java/src/atv2/java/atv2.java
@@ -0,0 +1,11 @@
+package atv2.java;
+
+public class atv2 {
+	 public static void main(String[] args) {
+		 int[] num = new int[100];
+		 for(int i=0; i<100;i++) {
+			 num[i]=i+1;
+			 System.out.println(num[i]);
+		 }
+	 }
+}
