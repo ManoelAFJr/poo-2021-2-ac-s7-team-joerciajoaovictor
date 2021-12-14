ad8b6490be21499268429cf598a1363d3e12e0f9
João Victor Gomes <joao-iccz@hotmail.com>
2021-12-14 16:57:07 -0300

    Adicionando questão 3 página 95

---
+++ atv3.java/src/atv3/java/atv3.java
@@ -0,0 +1,12 @@
+package atv3.java;
+
+public class atv3 {
+	 public static void main(String[] args) {
+		 int[] num = new int[100];
+		 int tamanho = num.length;
+		 for(int i=0; i<=num.length-1;i++) {
+			 num[i]=i;
+			 System.out.println(num[i]);
+		 }
+	 }
+}
