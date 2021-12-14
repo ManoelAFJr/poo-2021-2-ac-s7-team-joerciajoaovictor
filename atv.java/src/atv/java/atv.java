42d98ceeba2d9457c3dc8078121e6476d3a97079
João Victor Gomes <joao-iccz@hotmail.com>
2021-12-14 16:51:26 -0300

    Adicionando questão 1 pagina 93

---
+++ atv.java/src/atv/java/atv.java
@@ -0,0 +1,11 @@
+package atv.java;
+
+public class atv {
+ public static void main(String[] args) {
+	 int[] num = new int[100];
+	 for(int i=0; i<=99;i++) {
+		 num[i]=i;
+		 System.out.println(num[i]);
+	 }
+ }
+}
